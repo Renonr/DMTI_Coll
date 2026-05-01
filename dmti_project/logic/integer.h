74ee4fb8ff687f1587dd0472a0edb4040eaedc9d
IvanKovalev <v.a.kovalev2004@mail.ru>
2026-05-01 15:37:48 +0300

    Update 3

--- dmti_project/logic/integer.h
+++ dmti_project/logic/integer.h
@@ -1,12 +1,32 @@
 #ifndef INTEGER_H
 #define INTEGER_H
 
+#include "number_structures.h"
 #include "natural.h"
 
 class Integer : public Natural
 {
 public:
     Integer();
-};
 
+    Number ABS_Z_N(const IntegerNumber &num);
+
+    int SGN_Z_D(const IntegerNumber &num);
+
+    IntegerNumber MUL_ZM_Z(const IntegerNumber &num);
+
+    IntegerNumber TRANS_N_Z(const Number &num);
+
+    Number TRANS_Z_N(const IntegerNumber &num);
+
+    IntegerNumber ADD_ZZ_Z(const IntegerNumber &num1, const IntegerNumber &num2);
+
+    IntegerNumber SUB_ZZ_Z(const IntegerNumber &num1, const IntegerNumber &num2);
+
+    IntegerNumber MUL_ZZ_Z(const IntegerNumber &num1, const IntegerNumber &num2);
+
+    IntegerNumber DIV_ZZ_Z(const IntegerNumber &num1, const IntegerNumber &num2);
+
+    IntegerNumber MOD_ZZ_Z(const IntegerNumber &num1, const IntegerNumber &num2);
+};
 #endif // INTEGER_H
--- dmti_project/logic/rational.h
+++ dmti_project/logic/rational.h
@@ -1,12 +1,29 @@
 #ifndef RATIONAL_H
 #define RATIONAL_H
 
+#include "number_structures.h"
 #include "integer.h"
 
 class Rational : public Integer
 {
 public:
     Rational();
+
+    RationalNumber RED_Q_Q(const RationalNumber &frac);
+
+    bool INT_Q_B(const RationalNumber &frac);
+
+    RationalNumber TRANS_Z_Q(const IntegerNumber &num);
+
+    IntegerNumber TRANS_Q_Z(const RationalNumber &frac);
+
+    RationalNumber ADD_QQ_Q(const RationalNumber &frac1, const RationalNumber &frac2);
+
+    RationalNumber SUB_QQ_Q(const RationalNumber &frac1, const RationalNumber &frac2);
+
+    RationalNumber MUL_QQ_Q(const RationalNumber &frac1, const RationalNumber &frac2);
+
+    RationalNumber DIV_QQ_Q(const RationalNumber &frac1, const RationalNumber &frac2);
 };
 
 #endif // RATIONAL_H
