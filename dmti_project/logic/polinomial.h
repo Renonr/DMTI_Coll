f54c80386996121020cee0a8dea56ea0458d78ab
IvanKovalev <v.a.kovalev2004@mail.ru>
2026-05-06 14:04:42 +0300

    Static

--- dmti_project/logic/polinomial.h
+++ dmti_project/logic/polinomial.h
@@ -9,31 +9,31 @@ class Polinomial : public Rational
 public:
     Polinomial();
 
-    PolynomialNumber ADD_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
+    static PolynomialNumber ADD_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
 
-    PolynomialNumber SUB_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
+    static PolynomialNumber SUB_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
 
-    PolynomialNumber MUL_PQ_P(const PolynomialNumber &p, const RationalNumber &q);
+    static PolynomialNumber MUL_PQ_P(const PolynomialNumber &p, const RationalNumber &q);
 
-    PolynomialNumber MUL_Pxk_P(const PolynomialNumber &p, const Number &k);
+    static PolynomialNumber MUL_Pxk_P(const PolynomialNumber &p, const Number &k);
 
-    RationalNumber LED_P_Q(const PolynomialNumber &p);
+    static RationalNumber LED_P_Q(const PolynomialNumber &p);
 
-    Number DEG_P_N(const PolynomialNumber &p);
+    static Number DEG_P_N(const PolynomialNumber &p);
 
-    RationalNumber FAC_P_Q(const PolynomialNumber &p);
+    static RationalNumber FAC_P_Q(const PolynomialNumber &p);
 
-    PolynomialNumber MUL_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
+    static PolynomialNumber MUL_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
 
-    PolynomialNumber DIV_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
+    static PolynomialNumber DIV_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
 
-    PolynomialNumber MOD_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
+    static PolynomialNumber MOD_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
 
-    PolynomialNumber GCF_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
+    static PolynomialNumber GCF_PP_P(const PolynomialNumber &p1, const PolynomialNumber &p2);
 
-    PolynomialNumber DER_P_P(const PolynomialNumber &p);
+    static PolynomialNumber DER_P_P(const PolynomialNumber &p);
 
-    PolynomialNumber NMR_P_P(const PolynomialNumber &p);
+    static PolynomialNumber NMR_P_P(const PolynomialNumber &p);
 };
 
 #endif // POLINOMIAL_H
